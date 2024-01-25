2c43fbfb636f64111cb40538dccf91ee16ded071
Ivan Gonzalez <hal308366@gmail.com>
2024-01-25 11:15:55 -0800

    Crear, actualizar y eliminar

--- services/user.service.js
+++ services/user.service.js
@@ -2,20 +2,25 @@ const { models } = require('./../libs/sequelize');
 class UserService {
   constructor() {}
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
   async find() {
     const rta = await models.User.findAll();
     return rta;
   }
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    return user;
   }
   async update(id, changes) {
-    return { id, changes };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
   async delete(id) {
-    return { id };
+    const user = await this.findOne(id);
+    await user.destroy();
   }
 }
 module.exports = UserService;
